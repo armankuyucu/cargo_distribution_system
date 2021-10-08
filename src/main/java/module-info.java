70884d805060ff1c8857a0d50f215c613e7cb009
190201099 <190201099@kocaeli.edu.tr>
2021-10-08 03:52:50 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.yazlab1_proje1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.yazlab1_proje1 to javafx.fxml;
+    exports com.example.yazlab1_proje1;
+}
